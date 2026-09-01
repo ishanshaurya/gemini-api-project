5612205f83fd8ef677044c02cbfa767f034dc20b
Shaurya Ishan <ishannshaurya@gmail.com>
2026-09-01 20:07:48 +0530

    Initial commit: LLM API setup

---
+++ main.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Explain what an API is in one sentence."
+)
+
+print(response.text)
